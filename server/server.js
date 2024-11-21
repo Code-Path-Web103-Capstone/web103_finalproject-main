98dcefe7829cc97945c51314a9b7927df21c1129
Heider <heider2112@gmail.com>
2024-11-20 19:16:10 -0500

    make the server work by deleting path join

--- server/server.js
+++ server/server.js
@@ -12,18 +12,18 @@ import uploadRouter from "./routes/upload.js";
 
 dotenv.config();
 
-// Define __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Catch-all route to serve the React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+// // Define __dirname
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+//
+// const app = express();
+//
+// app.use(express.static(path.join(__dirname, 'dist')));
+//
+// // Catch-all route to serve the React app
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// });
 
 // CORS configuration
 const corsOptions = {
