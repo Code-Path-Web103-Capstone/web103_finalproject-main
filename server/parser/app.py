4b15e69c5ac1579c34d6eae61b11e22a100b6ee6
Heider <heider2112@gmail.com>
2024-11-20 21:46:20 -0500

    maybe the upload has a porblem

--- server/parser/app.py
+++ server/parser/app.py
@@ -52,7 +52,7 @@ def execute_parser_tdtest():
 async def upload_file(file: UploadFile = File(...)):
     upload_dir = './data'
     os.makedirs(upload_dir, exist_ok=True)
-    file_path = os.path.join(upload_dir, 'STATEMENT.pdf')
+    file_path = os.path.join(upload_dir, file.filename)
 
     try:
         with open(file_path, "wb") as buffer:
